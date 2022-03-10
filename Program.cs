3ebe8d247505c1b879b7248fb1f6ad923f6c0e87
riannf28 <riannurfauzi@student.telkomuniversity.ac.id>
2022-03-10 13:30:26 +0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace tpmodul3_1302200040
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
